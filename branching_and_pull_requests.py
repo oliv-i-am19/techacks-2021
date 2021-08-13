2ecb903bdc3d90a5823a7d4d83e2e84685e9831d
Olivia Ma <olivia.ma@myxing.net>
2021-08-13 19:45:26 -0400

    branching tutorial

---
+++ branching_and_pull_requests.py
@@ -0,0 +1,13 @@
+# Branching and Pull Requests
+
+# You should use branches to separate working code from buggy/unfinished code. When you create a new branch, it will contain all of the files the parent
+# branch contained. Whatever changes you make to the new branch will not appear on the parent branch until you open a pull request and your changes get
+# approved.
+# Make sure to pull from the remote repo before creating a new branch
+
+# Enter git checkout -b <branch name> - This creates a new branch, named <branch name>, and navigates to it. All changes you make will now exist on the
+# new branch.
+# When you're done with your changes, add, commit, and push your changes as usual.
+# On the remote repo, click on the tab called "Pull requests" and click "New pull request"
+# Select which branches you want to compare, AKA which branches you want to merge. Click "Create pull request"
+# The owner of the repo will have to review your changes and either approve your pull request (at which point the branches will be merged), or reject it
