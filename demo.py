61e2ede7bbc0bd4e3ca66f7de8906a506cd6489c
Anisha <anisha.j149@gmail.com>
2021-08-14 16:35:35 -0400

    x = 1234

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@
 # For example, if I write "x=5" on line 6 in my local repo, but my collaborator already pushed her changes that said "x=10" on line 6 to the remote repo,
 # when I push my changes, Git will not know which version to keep, and will result in a merge conflict
 
-x=5
+x=1234
 
 # Another way merge conflicts can occur is if you merge your branch with another, but there are differences between the branches that Git cannot resolve
