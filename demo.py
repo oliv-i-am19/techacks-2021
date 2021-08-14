eeab719850fe62b5d22f340393d3ad2341ac3eb4
Olivia Ma <olivia.ma@myxing.net>
2021-08-14 15:34:35 -0400

    merge conflict demo

---
+++ demo.py
@@ -0,0 +1,6 @@
+# Merge conflicts occur when 2 people make different changes to one location in a file.
+
+# For example, if I write "x=5" on line 5 in my local repo, but my collaborator already pushed her changes that said "x=10" on line 5 to the remote repo,
+# when I push my changes, Git will not know which version to keep, and will result in a merge conflict
+
+x=5
